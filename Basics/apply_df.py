e2b2d640a2e4f48e788a3f368fa64c88ee4f1dfb
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-02-09 17:24:17 +0530

    updated with some basic text processing

--- Basics/apply_df.py
+++ Basics/apply_df.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env/ conda:"base"
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
+
+import re
+from collections import namedtuple
 
 import pandas as pd
-import re
 
-from textblob import TextBlob
+from textblob import TextBlob, Word
 
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import sent_tokenize
 
-sample ="'I am a student from the University of Alabama.\
+
+words = namedtuple("correct_spells", ["student", "Philosophy", "improve", "professor", "difficult"])
+
+sample = "I am a studant from the University of Alabama.\
  I was born in Ontario, Canada and I am a huge fan of the United States.\
- I am going to get a degree in Philosophy to improve my chances of becoming a Philosophy professor.\
+ I am going to get a degree in Philosophy to improv my chances of becoming a Phylosopi professer.\
  I have been working towards this goal for 4 years.\
  I am currently enrolled in a PhD program.\
- It's very difficult, but I'm confident that it will be a good decision'" 
+ It's very dificult, but I'm confident that it will be a good decision"
+
 
 if __name__ == "__main__":
-    
-    re_clean = re.sub(r'[^\w\s]', '', sample.lower())
- 
     df = pd.DataFrame(sent_tokenize(sample), columns=['text'])
-
     df['text'] = df['text'].str.replace('[^\w\s]', '', regex=True)
-    
+
     # -- now remove stopwords --
     stoppies = stopwords.words('english')
-
     df['no_stopwords'] = df['text'].apply(lambda x: " ".
-            join(x for x in str.split(x.lower()) if x not in stoppies)
-        )
+                                          join(x for x in str.split(x.lower())
+                                               if x not in stoppies
+                                               )
+                                          )
     # -- correct spellings --
-    df['correct_spells'] = df['no_stopwords'].apply(lambda x: 
-            str(TextBlob(x).correct())
-        )
-    print(df) 
+    df['correct_spells'] = df['no_stopwords'].apply(lambda x:
+                                                    str(TextBlob(x).correct())
+                                                    )
+    # -- No change: if we keep first occurance of philosophy word as Phylosphi, we get incorrect words.  
+    # -- corrections: studant, dificult, improv, professer got corrected, Phylosopi.
 
-    del df, re_clean, stoppies
+    # -- Lemmatization --
+    df['lemmatized'] = df['correct_spells'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+    print(df)
+
+    del df, stoppies
