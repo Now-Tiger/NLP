0bc4d700b8158b969e3e62dd0f0abbf1f858118b
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-03-02 01:46:29 +0530

    updated with some methods

--- ChatBot/src/hr-bot.py
+++ ChatBot/src/hr-bot.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env/ conda: "base"
-# -*- coding: utf-8- 
+# -*- coding: utf-8-
 import os
+import re
+import string
+import random
+import unicodedata
 from urllib.request import urlopen
 from collections import defaultdict
-import re, string, random, unicodedata
 
+import wikipedia as wk
 from nltk import pos_tag
 from bs4 import BeautifulSoup as bs
 from nltk.corpus import (wordnet, stopwords)
@@ -23,8 +27,15 @@ FILENAME: str = 'HR.txt'
 URL: str = "https://www.whatishumanresource.com/human-resource-management"
 
 
-welcome_input = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-welcome_response = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+welcome_input = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
+welcome_response = ["hi", "hey", "*nods*", "hi there",
+                    "hello", "I am glad! You are talking to me"]
+
+
+def welcome(user_response):
+    for word in user_response.split():
+        if word.lower() in welcome_input:
+            return random.choice(welcome_response)
 
 
 def maybe_download() -> None:
@@ -55,16 +66,17 @@ def process_text() -> None:
     punctuation_dict = dict((ord(punct), None) for punct in string.punctuation)
     word_tokens = word_tokenize(raw.translate(punctuation_dict))
     for word in word_tokens:
-        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        new_word = unicodedata.normalize('NFKD', word).encode(
+            'ascii', 'ignore').decode('utf-8', 'ignore')
         new_words.append(new_word)
     rmv = []
     for w in new_words:
-        text = re.sub('&lt;/?.*?&gt;','&lt;&gt;',w)
+        text = re.sub('&lt;/?.*?&gt;', '&lt;&gt;', w)
         text = re.sub(r'[0-9]', '', w)
         rmv.append(text)
     stoppies = stopwords.words('english')
     rmvd = [word for word in rmv if word not in stoppies]
-    tag_map = defaultdict(lambda : wordnet.NOUN)
+    tag_map = defaultdict(lambda: wordnet.NOUN)
     tag_map['J'] = wordnet.ADJ
     tag_map['V'] = wordnet.VERB
     tag_map['R'] = wordnet.ADV
@@ -77,11 +89,40 @@ def process_text() -> None:
     return lemma_list
 
 
-def welcome(user_response):
-    for word in user_response.split():
-        if word.lower() in welcome_input:
-            return random.choice(welcome_response)
+def generate_response(user_response: str = None) -> None:
+    with open(PATH + FILENAME, 'r') as file:
+        raw = " ".join(x.strip().lower() for x in file.readlines())
+    bot_resp = ''
+    sent_tokens = sent_tokenize(raw)
+    sent_tokens.append(user_response)
+    tfidf_vec = TfidfVectorizer(tokenizer=process_text, stop_words='english')
+    tfidf = tfidf_vec.fit_transform(sent_tokens)
+    values = linear_kernel(tfidf[-1], tfidf)
+    indexes = values.argsort()[0][-2]
+    flat = values.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if req_tfidf == 0 or "tell me about" in user_response:
+        print("checking wikipedia...")
+        if user_response:
+            bot_resp = wikipedia_data(user_response)
+            return bot_resp
+        else:
+            bot_resp += sent_tokens[indexes]
+            return bot_resp
+
+
+def wikipedia_data(input) -> str:
+    regex = re.search('tell me about (.*)', input)
+    try:
+        if regex:
+            topic = regex.group(1)
+            wiki = wk.summary(topic, sentences=3)
+            return wiki
+    except Exception as err:
+        print(f"No content has been found: \n{err}")
 
 
 if __name__ == "__main__":
     lemma = process_text()  # <- working properly
+    generate_response()
